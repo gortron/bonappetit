63b6395a9f6db601e79a2cf031ad587c4134fe2d
gortron <gordon.lanza@gmail.com>
2020-02-07 09:54:12 -0800

    set up rails backend

---
+++ db/seeds.rb
@@ -0,0 +1,22 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Player.delete_all
+
+Player.create(name: "ned-el-ch", score: 10000)
+Player.create(name: "gawaxx", score: 9000)
+Player.create(name: "gortron", score: 10000)
+Player.create(name: "gortron", score: 1000)
+Player.create(name: "gortron", score: 2000)
+Player.create(name: "liko", score: 11000)
+Player.create(name: "gortron", score: 3000)
+Player.create(name: "gortron", score: 4000)
+Player.create(name: "gortron", score: 5000)
+Player.create(name: "gortron", score: 6000)
+Player.create(name: "gortron", score: 7000)
+Player.create(name: "gortron", score: 8000)
+Player.create(name: "gortron", score: 9000)
